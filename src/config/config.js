fcd521dac2933a29c3e1a3aa5f151b69b14b5886
julio <juliioalbert98@gmail.com>
2020-06-25 23:17:45 -0500

    Segundo

--- src/config/config.js
+++ src/config/config.js
@@ -13,7 +13,7 @@ process.env.TIEMPO= process.env.TIEMPO || '1hr';
 if(process.env.NODE_ENV === 'dev'){
     URL='mongodb://localhost:27017/cafe';
 }else{
-    URL='mongodb+srv://juli:juli@cluster0-ugleq.mongodb.net/cafe';
+    URL = process.env.MONGO= process.env.MONGO ;
 }
 
 process.env.URLDB = URL;
